5363adf956843f748f0b55a76f850403179ac5f9
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-11-21 11:22:10 +0300

    log init data in Errorpage

--- src/pages/Errorpage/Errorpage.jsx
+++ src/pages/Errorpage/Errorpage.jsx
@@ -1,5 +1,7 @@
 import {useLocation, useNavigate} from 'react-router-dom';
 
+import {tg} from '../hooks/useTelegram';
+
 import {CustomContainer} from '../../components/UI/CustomContainer';
 import {PrimaryHeading} from '../../components/UI/PrimaryHeading';
 import Warning from '../../assets/images/warning.png';
@@ -10,6 +12,7 @@ import classes from './Errorpage.module.scss';
 
 
 export const Errorpage = ({status}) => {
+    console.log('INITDATA', tg.initData);
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -21,6 +24,7 @@ export const Errorpage = ({status}) => {
         <CustomContainer>
             <section className={classes.errorpage}>
                 <BackArrow className={classes.button} onClick={handleCLick}/>
+                {`INITDATA is ${tg.initData}`}
                 <PrimaryHeading>{location.state || status}</PrimaryHeading>
                 <p className={classes.message}>{ERROR_MESSAGE[location.state] ||ERROR_MESSAGE[status]}</p>
                 <img src={Warning} alt="Декоративное изображение"/></section>

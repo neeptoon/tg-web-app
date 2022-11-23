072adf24f5bd32c8c58c593b70f3c2ffc9515d7a
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-11-23 15:33:34 +0300

    fix fontsize of table

--- src/services/config.js
+++ src/services/config.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 import {tg} from '../hooks/useTelegram';
 
-console.log('INITDATA', tg.initData)
-
 const token = window.btoa(tg.initData);
+
+// const AUTH_TOKEN = 'Bearer' +
+//     '  cXVlcnlfaWQ9QUFGdEtWZ1JBQUFBQUcwcFdCRnZudVJmJnVzZXI9JTdCJTIyaWQlMjIlM0EyOTA5OTA0NDUlMkMlMjJmaXJzdF9uYW1lJTIyJTNBJTIyQWxleGFuZHIlMjIlMkMlMjJsYXN0X25hbWUlMjIlM0ElMjJMb3BhdGluJTIyJTJDJTIybGFuZ3VhZ2VfY29kZSUyMiUzQSUyMnJ1JTIyJTdEJmF1dGhfZGF0ZT0xNjY3MzAzNDE3Jmhhc2g9Yzg0NTdhNTg4OWMxNjc0ZjFjZmE0NGJhMmRjZDVkMWQ4YzIzOWFmN2UyZmU4Yzg2NmEwMDBiZGQyNmIyM2U5Mw==';
 
 const AUTH_TOKEN = `Bearer ${token}`;
 export const instance = axios.create({
